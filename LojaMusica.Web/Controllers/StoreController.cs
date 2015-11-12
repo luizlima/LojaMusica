56a84ca79b1c86d8b41f6562b9398851911ce172
Luiz Oscar Lemos de Lima <luizoscar.lima@gmail.com>
2015-11-11 22:20:55 -0200

    [Add] adição de dois controllers: Home e Store [Add] adição de imagens [Change] Mudança nos estilos presentes no site.css [Change] Mudança na view _layout

---
+++ LojaMusica.Web/Controllers/StoreController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaMusica.Web.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Browse()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
