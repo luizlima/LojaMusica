16ded858febb0c239c4f78a664b6330e91df5269
Luiz Oscar Lemos de Lima <luizoscar.lima@gmail.com>
2015-12-07 17:54:29 -0200

    [add] classe para artista [fix] atributos adicionados nas classes de album e genre

--- LojaMusica.Web/Models/Album.cs
+++ LojaMusica.Web/Models/Album.cs
@@ -7,8 +7,14 @@ namespace LojaMusica.Web.Models
 {
     public class Album
     {
+        public int Id { get; set; }
+        public int GenreId { get; set; }
+        public int ArtistId { get; set; }
         public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
 
         public Genre Genre { get; set; }
+        public Artist Artist { get; set; }
     }
 }
--- LojaMusica.Web/Models/Artist.cs
+++ LojaMusica.Web/Models/Artist.cs
@@ -5,11 +5,9 @@ using System.Web;
 
 namespace LojaMusica.Web.Models
 {
-    public class Genre
+    public class Artist
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public List<Album> Albums { get; set; }
     }
 }
